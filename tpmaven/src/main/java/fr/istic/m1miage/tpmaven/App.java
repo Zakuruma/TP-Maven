bf06057f7d6efae7f4e5924fbc89801d7d0e72b9
10008014 <10008014@e107m06.istic.univ-rennes1.fr>
2014-01-22 15:05:03 +0100

    Commit initial

---
+++ tpmaven/src/main/java/fr/istic/m1miage/tpmaven/App.java
@@ -0,0 +1,14 @@
+package fr.istic.m1miage.tpmaven;
+/**
+ * @author 10008014
+ * Hello world!
+ * Class App
+ */
+public class App {
+    /**
+     * @param args : args de type final
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
